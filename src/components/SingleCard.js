43566ff10eff5dfb4754b02b044ef7e5583181ce
Anku07 <as19961107@gmail.com>
2024-04-10 15:00:45 +0530

    formatting components file

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,24 +1,20 @@
-import  './SingleCard.css'
+import "./SingleCard.css";
 
-export default function SingleCard( {card, handleChoice, flipped} ) {
-    const handleClick = () => {
-        handleChoice(card)
-    }
+export default function SingleCard({ card, handleChoice, flipped }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
   return (
-    
-      <div className='card' >
-            <div className={flipped ? "flipped" : ""}>
-              <img className='front' src ={card.src} alt='card front'/>
-              <img  
-              className='back' 
-              src='/img/cover.png'
-              onClick = {handleClick}
-              alt='card back' 
-              />
-            </div>
-          </div>
-  )
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
 }
-
-
-
